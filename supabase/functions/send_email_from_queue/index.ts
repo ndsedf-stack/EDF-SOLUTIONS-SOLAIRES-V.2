1b1cb596405caf629f498956561f727d2a3975fa
ndsedf-stack <ndsedf@gmail.com>
2026-02-08 17:08:39 +0100

    fix: force new esm.sh versions to bypass cache

--- supabase/functions/send_email_from_queue/index.ts
+++ supabase/functions/send_email_from_queue/index.ts
@@ -1,7 +1,7 @@
 // @ts-nocheck
 import { serve } from "https://deno.land/std@0.192.0/http/server.ts";
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
-import { Resend } from "https://esm.sh/resend";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2.47.10";
+import { Resend } from "https://esm.sh/resend@4.0.0";
 
 serve(async (req) => {
   try {
